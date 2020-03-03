07d7da49b1f8b1c45fb14a4605f2fe30f6ed501b
Gobinathnagaraj <51577593+Gobinathnagaraj@users.noreply.github.com>
2020-03-03 17:02:19 +0530

    updated files are added
    complete routing and validation

--- validate.js
+++ validate.js
@@ -1,16 +1,6 @@
-"use strict";
-var $ = function(id);
-    return document.getElementById(id);
-
-var submit = function(){
-    var Name = $("name").value;
-    var Age = $("age").value;
-    var isValid = true;
-    
-if (Name === "") {
-    alert("The Name field is required");
-    isValid = false;
-} else { $("error1").nodeValue = "";}
-};
-
-
+var name = document.getElementById(name);
+var numbers = function(){
+    if (name === "") {
+        alert("This field is required");
+    }
+}
